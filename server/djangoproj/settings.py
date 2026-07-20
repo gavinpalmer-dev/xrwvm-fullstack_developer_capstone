cc94e6bbff57de30fecead1a883c400da012ee6e
gavinpalmer-dev <gavin_palmer_gp@yahoo.com>
2026-07-20 12:46:48 -0400

    Completed module 3

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -32,9 +32,11 @@ ALLOWED_HOSTS = [
     'localhost',
     'localhost:8000'
     'gavinpalmer-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'gavinpalmer-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://gavinpalmer-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://gavinpalmer-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://gavinpalmer-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
